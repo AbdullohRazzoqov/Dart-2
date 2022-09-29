de212e13b30ba473edd613aea28db1f103dcebce
Shohjahon <shohjahonrazzoqov@gmail.com>
2022-09-29 17:44:45 -0400

    constructor joined

---
+++ opp/constructor.dart
@@ -0,0 +1,55 @@
+void main() {
+  Car carOne = Car();
+  print(carOne);
+
+  print(User(
+      name: "Bobur", age: 25)); //Bunda name va age kiritish hohishiy bo'ldi.
+
+  print(UserTwo(
+      name:
+          'Bobur')); //Bunga name berish majburiy bo'ldi chunki buni required qilib elon qildik yani sanga qiymat keladi deb.
+
+  UserTwo UserAge = UserTwo.UserAge(age: 30);
+  print(UserAge);
+}
+
+//Constructorlar
+// 1 - Default Constructor (Parametrsiz)
+// 2 - Parametrli Constructor
+// 3 - Named Constructorlar
+
+// Default va Parametrli constructor bitta joyda bo'lishi mumkin emas
+// lekin Named Constructordan hohlagancha kirgazish mumkin
+
+//Defautl Constructor
+class Car {
+  int? speed;
+  String name = "Tesla";
+  Car() {
+    print("Defautl Constructor");
+  }
+}
+
+// 2 - Parametrli Constructor
+class User {
+  String? name;
+  int? age;
+  User({this.name, this.age}) {
+    print("${this.name} ${this.age}");
+  }
+}
+
+// 3 - Named Constructor
+// Dartda Medhod Overloading yo'q uning o'rniga named Consructorni ko'rsatish mumkin
+// Agar Paramertlar bajburiy bo'lsa lekin barchsini olishga hojat bo'lmasa shunday qilib keraglisin sihlatish mumkin
+class UserTwo {
+  String? name;
+  int? age;
+  UserTwo({required this.name, this.age}) {
+    print("${this.name} ${this.age}");
+  }
+
+  UserTwo.UserAge({required this.age}) {
+    print("User Age ${this.age}");
+  }
+}
