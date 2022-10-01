bcb5270dcbd143e19c37bc65d1092bc7b1eb14ad
Shohjahon <shohjahonrazzoqov@gmail.com>
2022-09-30 20:56:14 -0400

    Getter and Setter added

--- opp/private_field/private.dart
+++ opp/private_field/private.dart
@@ -1,6 +1,21 @@
 import 'private_field.dart';
 
-void main() {
+void main() { 
+  //   _ => Underscore
   Leptop lep = Leptop();
-  print(lep.color);   //bu yerda cost yo'q chunki biz uni private qildik
+  print(lep.color); //bu yerda cost yo'q chunki biz uni private qildik
+  //shu yerdan costga qiymat berish mumkin lekin bu degani kiyinchalik uning qiymatini o'zgartira olaman degani emas
+  LeptopMac lepMac =
+      LeptopMac("Mac book", 3000); //Bu yerdan unga qiymat berdik lekin
+  print(lepMac.model); //Bu yerdan chiqmadi
+
+  //Getter
+  print(lepMac.costGetter); //Endi buni shunday qilib ko'rishimiz mumkin
+
+  //Setter
+  lepMac.costSetter = 5000;
+  print(lepMac.costGetter);
+
+  //Getter Fuction
+  lepMac.setFunHello;
 }
--- opp/private_field/private_field.dart
+++ opp/private_field/private_field.dart
@@ -1,14 +1,43 @@
 //private field -> o'zimiz ishlatishni hohlagan o'zgaruvchilarimizni va boshqalar teginishini hohlamagan o'zgaruvchilarimizni,
 //umuman olganda bir o'zgaruvchimizning qiymatini o'zgartirishni hohlamaymiz adashib bo'lsa ham almashtirib qo'ymaslik uchun private field qilamiz
-//agarda biz faqat bir sahifada ishlaydigan bo'lsak priveate qilishdan deyarli biz uchun foida yo'q
+//agarda biz faqat bir sahifada ishlaydigan bo'lsak priveate qilishdan deyarli biz uchun foidasi yo'q
+//Function bo'lsin o'zgaruvchi bo'lsin vaziatga qarab private qilib ishlatishimiz mumkin
 
 void main() {
   Leptop notebook = Leptop();
-  print(notebook._cost);  // bunda 2000 chiqadi. Bir sahifada ekanligi uchun
+  print(notebook._cost); // bunda 2000 chiqadi. Bir sahifada ekanligi uchun
 }
 
 class Leptop {
   int _cost = 2000;
   String model = "Lenovo";
   String color = "Green";
+}
+
+class LeptopMac {
+  String? model;
+  int? _cost;
+  LeptopMac(this.model, this._cost) {}
+
+//Boshqa sahifadan o'qiy olmasin lekin Men bergan getter setterlar bilan o'qiy olsin
+//Men private fieldlarimni qiymatini ishlatmoqchi bo'lsam Getterga extiyoj sezaman
+//Qiymatini o'zgartirmoqchi bo'lsam Setterga extiyoj sezaman
+
+//Getter
+  int get costGetter =>
+      _cost!.toInt(); //Huddi shunday qilib chiqarib olishimiz mumkin
+
+//Setter
+
+  void set costSetter(int newCost) => _cost =
+      newCost; //Bu yerda endi qiymmatni ham o'zgartirish imkoniga ega bo'lamiz
+
+//Function Private field
+  void _setHello() {
+    print("Hello");
+  }
+
+  void get setFunHello {
+    _setHello();
+  }
 }
